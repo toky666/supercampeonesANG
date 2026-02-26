5152fc724efba6961fcf2439adc613c4444941d8
TokyDev <yokycoronelchavez@gmail.com>
2026-02-26 15:54:08 -0400

    Roles: create services roles fase 1

--- src/app/routes/sistema/roles/roles.ts
+++ src/app/routes/sistema/roles/roles.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { PageHeader } from '@shared';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { RolesServicesService } from '../../services/roles-services.service';
+import { rolesInterfaz } from '../../Interfaces/rolesInterfaz';
+//PARA LLAMAR JAVASCRIPT/////
+declare let alertify: any;
 @Component({
   selector: 'app-sistema-roles',
-  imports: [PageHeader],
+  imports: [
+    PageHeader,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './roles.html',
-  styleUrl: './roles.scss'
+  standalone: true,
+  styleUrl: './roles.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SistemaRoles {
+  @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
 
+  private dialog = inject(MatDialog);
+  private dataService = inject(RolesServicesService);
+ 
+
+
+  form = new FormGroup({
+    name: new FormControl<string>(''), // ahora es siempre string
+  });
+
+  openDialog() {
+    this.dialog.open(this.dialogTemplate, { width: '400px' });
+  }
+
+  saveData() {
+    if (this.form.valid) {
+      this.dataService.save(this.form.value as rolesInterfaz).subscribe(
+        (data) => {
+          console.log('Se agrego el rol a la lista');
+        },
+        (error) => {
+          console.log(error);
+          console.log('Se desconecto el servidor de la lista');
+        },
+      );
+    } else {
+      console.log('Llene todos los campos');
+    }
+  }
 }
